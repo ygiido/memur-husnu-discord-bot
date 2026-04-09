3f94b430287995afeacc3322f27cec5ac4d77eae
Yiğit <82116741+ygiido@users.noreply.github.com>
2026-04-09 18:28:04 +0300

    duyur.js

---
+++ commands/duyur.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('duyur')
+        .setDescription('Belirtilen kanala everyone etiketiyle duyuru yapar.')
+        .addChannelOption(option => option.setName('kanal').setDescription('Kanal').setRequired(true))
+        .addStringOption(option => option.setName('mesaj').setDescription('Mesaj').setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        const kanal = interaction.options.getChannel('kanal');
+        const mesaj = interaction.options.getString('mesaj');
+        await kanal.send(`@everyone\n\n**Duyuru:** ${mesaj}`);
+        await interaction.reply({ content: 'Duyuru gönderildi.', ephemeral: true });
+    },
+};
